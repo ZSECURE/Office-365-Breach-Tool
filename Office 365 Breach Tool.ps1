3f8209a066da6871d8ccaf49b4c1e94e823a8b26
M1st3r3 <46941177+M1st3r3@users.noreply.github.com>
2019-08-19 16:43:53 +0100

    Update Office 365 Breach Tool.ps1

--- Office 365 Breach Tool.ps1
+++ Office 365 Breach Tool.ps1
@@ -1,4 +1,4 @@
-﻿# =============================================================================
+# =============================================================================
 # File Name: Office 365 Breach Tool.ps1
 # =============================================================================
 # Name: Office 365 Breach Tool
@@ -40,6 +40,9 @@ Import-PSSession $session
 
 ## List mailboxes with their forwarders and exports to a csv
 Get-Mailbox -ResultSize Unlimited |  Where-Object {$_.ForwardingSmtpAddress -ne $null} | select UserPrincipalName,ForwardingSmtpAddress,DeliverToMailboxAndForward | Export-Csv -Path "$SaveDir$CompanyName SMTP_Forwarders.csv"
+
+## Lists Inbox Rules for All Users
+Get-Mailbox -ResultSize Unlimited | % { Get-InboxRule -Mailbox $_.Alias | Select Enabled,Name,Priority,From,SentTo,CopyToFolder,DeleteMessage,ForwardTo,MarkAsRead,MoveToFolder,RedirectTo,@{Expression={$_.SendTextMessageNotificationTo};Label="SendTextMessageNotificationTo"},MailboxOwnerId } | Export-Csv "$SaveDir$CompanyName Inbox_Rules.csv" -NoTypeInformation
 
 ##This creates a CSV for any newly created users in the last 7 days, this can be changed if needs be.
 Get-User -ResultSize Unlimited | where {$_.WhenCreated -gt (get-date).adddays(-7)} | ft Name,whenCreated –Autosize | Export-Excel "$SaveDir$CompanyName Newly_Created_Users.xlsx"
